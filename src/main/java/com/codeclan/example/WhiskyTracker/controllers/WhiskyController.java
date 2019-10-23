9169355baa3a4df56a8945a277bfb40e5eb92923
Joe Cooper <josephcooper3@gmail.com>
2019-10-23 18:37:56 +0100

    adds RequestParam endpoint for findWhiskiesByDistilleryAndAge, fixes infinite recursion

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,10 +1,23 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/whiskies")
 public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/distilleries")
+    @ResponseBody
+    public List<Whisky> findWhiskiesByDistilleryAndAge(@RequestParam String name, int age) {
+        return whiskyRepository.findWhiskiesByDistilleryAndAge(name, age);
+    }
 
 }
