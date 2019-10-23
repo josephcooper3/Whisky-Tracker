a6fed6e5f7ad8b149fc2b86a5e464472cffa56cc
Joe Cooper <josephcooper3@gmail.com>
2019-10-23 16:33:14 +0100

    adds findWhiskiesByDistilleryAndAge method

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface WhiskyRepository extends JpaRepository<Whisky, Long>, WhiskyRepositoryCustom {
     List<Whisky> findWhiskiesByYear(int year);
+    List<Whisky> findWhiskiesByAge(int age);
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -1,5 +1,43 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Whisky> findWhiskiesByDistilleryAndAge(String distilleryName, int whiskyAge) {
+        List<Whisky> result = null;
+
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria criteria = session.createCriteria(Whisky.class);
+
+            criteria.add(Restrictions.eq("age", whiskyAge));
+            criteria.createAlias("distillery", "distilleryAlias");
+            criteria.add(Restrictions.eq("distilleryAlias.name", distilleryName));
+
+            result = criteria.list();
+
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
